908a004378a532c289e8b53c6a4685a820ff8429
travenupai <ai.travenup@gmail.com>
2024-11-22 16:50:03 -0300

    atualizei crew.py

--- src/vidmarpestel/crew.py
+++ src/vidmarpestel/crew.py
@@ -1,18 +1,29 @@
+# crew.py
+ 
+import os
+import openai
+from dotenv import load_dotenv
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from dotenv import load_dotenv
-from crewai_tools import (SerperDevTool, ScrapeWebsiteTool, ScrapeElementFromWebsiteTool)
-from vidmarpestel.my_llm import MyLLM
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, ScrapeElementFromWebsiteTool
+from langchain_openai import ChatOpenAI
+
+# from vidmarpestel.my_llm import MyLLM
 load_dotenv()
+
+# Set up OpenAI API key
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY is not set in the environment variables.")
+
+openai.api_key = api_key
+
+# Initialize the LLM
+llm = ChatOpenAI(model_name="gpt-4o-mini", openai_api_key=api_key)
+
 search_tool = SerperDevTool()
 scrape_tool = ScrapeWebsiteTool()
 scrape_element_tool = ScrapeElementFromWebsiteTool()
-
-# Uncomment the following line to use an example of a custom tool
-# from vidmarpestel.tools.custom_tool import MyCustomTool
-
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
 
 @CrewBase
 class VidmarpestelCrew():
@@ -28,7 +39,7 @@ class VidmarpestelCrew():
 			allow_delegation=False,
 			allow_interruption=True,  # Permite interrupções para reagir rapidamente a mudanças nos produtos dos concorrentes
 			allow_fallback=True,      # Permite fallback caso precise de um especialista em tecnologia ou satisfação do cliente
-			llm=MyLLM.gpt4o_mini_2024_07_18
+			llm=llm
 		)
 
 	@agent
@@ -41,7 +52,7 @@ class VidmarpestelCrew():
 			allow_delegation=False,
 			allow_interruption=True,  # Permite interrupções para reagir rapidamente a mudanças nos produtos dos concorrentes
 			allow_fallback=True,      # Permite fallback caso precise de um especialista em tecnologia ou satisfação do cliente
-			llm=MyLLM.gpt4o_mini_2024_07_18
+			llm=llm
 		)
 
 	@agent
@@ -54,7 +65,7 @@ class VidmarpestelCrew():
 			allow_delegation=False,
 			allow_interruption=True,  # Permite interrupções para reagir rapidamente a mudanças nos produtos dos concorrentes
 			allow_fallback=True,      # Permite fallback caso precise de um especialista em tecnologia ou satisfação do cliente
-			llm=MyLLM.gpt4o_mini_2024_07_18,
+			llm=llm
 		)
 
 	@agent
@@ -67,7 +78,7 @@ class VidmarpestelCrew():
 			allow_delegation=False,
 			allow_interruption=True,  # Permite interrupções para reagir rapidamente a mudanças nos produtos dos concorrentes
 			allow_fallback=True,      # Permite fallback caso precise de um especialista em tecnologia ou satisfação do cliente
-			llm=MyLLM.gpt4o_mini_2024_07_18,
+			llm=llm
 		)
   
 	@agent
@@ -80,7 +91,7 @@ class VidmarpestelCrew():
 			allow_delegation=False,
 			allow_interruption=True,  # Permite interrupções para reagir rapidamente a mudanças nos produtos dos concorrentes
 			allow_fallback=True,      # Permite fallback caso precise de um especialista em tecnologia ou satisfação do cliente
-			llm=MyLLM.gpt4o_mini_2024_07_18,
+			llm=llm
 		)
   
 	@agent
@@ -93,8 +104,7 @@ class VidmarpestelCrew():
 			allow_delegation=False,
 			allow_interruption=True,  # Permite interrupções para reagir rapidamente a mudanças nos produtos dos concorrentes
 			allow_fallback=True,      # Permite fallback caso precise de um especialista em tecnologia ou satisfação do cliente
-
-			llm=MyLLM.gpt4o_mini_2024_07_18,
+			llm=llm
 		)
 		
 
@@ -111,35 +121,40 @@ class VidmarpestelCrew():
 	def economic_analysis(self) -> Task:
 		return Task(
 			config=self.tasks_config['economic_analysis'],
-   			output_file='economic_agent.md'
+   			output_file='economic_agent.md',
+			guardrails=[{"output_format": "markdown"}, {"max_length": 20000}]
 		)
 
 	@task
 	def social_analysis(self) -> Task:
 		return Task(
 			config=self.tasks_config['social_analysis'],
-   			output_file='social_agent.md'
+   			output_file='social_agent.md',
+			guardrails=[{"output_format": "markdown"}, {"max_length": 20000}]
 		)
   
 	@task
 	def technological_analysis(self) -> Task:
 		return Task(
 			config=self.tasks_config['technological_analysis'],
-   			output_file='technological_agent.md'
+   			output_file='technological_agent.md',
+			guardrails=[{"output_format": "markdown"}, {"max_length": 20000}]
 		)
   
 	@task
 	def environmental_analysis(self) -> Task:
 		return Task(
 			config=self.tasks_config['environmental_analysis'],
-   			output_file='environmental_agent.md'
+   			output_file='environmental_agent.md',
+			guardrails=[{"output_format": "markdown"}, {"max_length": 20000}]
 		)
   
 	@task
 	def legal_analysis(self) -> Task:
 		return Task(
 			config=self.tasks_config['legal_analysis'],
-			output_file='legal_agent.md'
+			output_file='legal_agent.md',
+			guardrails=[{"output_format": "markdown"}, {"max_length": 20000}]
 		)
 
 
@@ -149,7 +164,8 @@ class VidmarpestelCrew():
 			agents=self.agents,  # Automatic collection of agents from decorated methods
 			tasks=self.tasks,     # Automatic collection of tasks from decorated methods
 			process=Process.sequential,
-			verbose=True
+			verbose=True,
+			memory=True  # Activate memory
         )
 
 
